156771b5395bc6dfc27457a409e1d848d1a54e56
wolferd-wolf <akilawolfs@gmail.com>
2026-08-03 10:53:22 +0530

    [TEKNIK] fix voxel face winding

--- scripts/world/chunk_mesher.gd
+++ scripts/world/chunk_mesher.gd
@@ -32,8 +32,8 @@ const FACE_VERTICES := [
 	[Vector3(1, 0, 0), Vector3(1, 0, 1), Vector3(1, 1, 1), Vector3(1, 1, 0)],
 	[Vector3(0, 0, 0), Vector3(0, 0, 1), Vector3(1, 0, 1), Vector3(1, 0, 0)],
 	[Vector3(0, 1, 1), Vector3(0, 1, 0), Vector3(1, 1, 0), Vector3(1, 1, 1)],
-	[Vector3(1, 0, 0), Vector3(0, 0, 0), Vector3(0, 1, 0), Vector3(1, 1, 0)],
-	[Vector3(0, 0, 1), Vector3(1, 0, 1), Vector3(1, 1, 1), Vector3(0, 1, 1)],
+	[Vector3(0, 0, 0), Vector3(1, 0, 0), Vector3(1, 1, 0), Vector3(0, 1, 0)],
+	[Vector3(1, 0, 1), Vector3(0, 0, 1), Vector3(0, 1, 1), Vector3(1, 1, 1)],
 ]
 
 
